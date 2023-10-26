58f2b0b156a6b3b294d883db65a3aa970038a9db
Imen Ben Atig <Imen.BENATIG@esprit.tn>
2023-10-26 09:35:41 +0100

    toolbar & navbar bugs fixing successfully

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,5 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.google.guava:guava:31.0.1-jre")
 
+
 }
--- app/src/main/java/com/example/gamer/HomeActivity.kt
+++ app/src/main/java/com/example/gamer/HomeActivity.kt
@@ -3,13 +3,17 @@ package com.example.gamer
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.example.gamer.databinding.HomeBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
 
@@ -18,33 +22,46 @@ class HomeActivity : AppCompatActivity() {
     private var nightMode: Boolean = false
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var editor: SharedPreferences.Editor
+
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
 
             binding = HomeBinding.inflate(layoutInflater)
             setContentView(binding.root)
 
-            binding.btnNews.setOnClickListener {
-                changeFragment(NewsFragment(), "News")
+
+          /*  val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.view_bar)
+            setSupportActionBar(toolbar)
+            val navView: BottomNavigationView = binding.bottomNavigation
+*/
+
+
+            val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+            val cartIcon = findViewById<ImageView>(R.id.shop)
+
+            bottomNavigation.setOnNavigationItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.item_news -> {
+                        goToFragment(NewsFragment())
+                        cartIcon.visibility = View.GONE
+                        return@setOnNavigationItemSelectedListener true
+                    }
+                    R.id.item_store -> {
+                        goToFragment(StoreFragment())
+                        cartIcon.visibility = View.VISIBLE
+                        return@setOnNavigationItemSelectedListener true
+                    }
+                    R.id.item_profile -> {
+                        goToFragment(ProfileFragment())
+                        cartIcon.visibility = View.GONE
+                        return@setOnNavigationItemSelectedListener true
+                    }
+                    else -> false
+                }
             }
-            val btnNews = findViewById<Button>(R.id.btnNews)
-            val btnEvents = findViewById<Button>(R.id.btnStore)
-            val btnProfile = findViewById<Button>(R.id.btnProfile)
-
-            btnNews.setOnClickListener {
-                changeFragment(NewsFragment(), "News")
-            }
-
-            btnEvents.setOnClickListener {
-                changeFragment(StoreFragment(), "Events")
-            }
-
-            btnProfile.setOnClickListener {
-                changeFragment(ProfileFragment(), "Change")
-            }
 
 
-                switchMode = findViewById(R.id.switchMode)
+            switchMode = findViewById(R.id.switchMode)
                 sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE)
                 nightMode = sharedPreferences.getBoolean("nightMode", false)
 
@@ -65,10 +82,14 @@ class HomeActivity : AppCompatActivity() {
                     }
                     editor.apply()
                 }
+            
             }
 
 
-
+    private  fun goToFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction().replace(R.id.fragment_container,fragment).commit()
+    }
         private fun changeFragment(fragment: Fragment, tag: String) {
             val fragmentManager = supportFragmentManager
             val transaction = fragmentManager.beginTransaction()
@@ -80,9 +101,9 @@ class HomeActivity : AppCompatActivity() {
                 transaction.addToBackStack(tag)
             }
 
-
-
         }
+
+
     }
 
 
